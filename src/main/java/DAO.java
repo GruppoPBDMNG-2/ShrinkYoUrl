543298311882e6a6911b8e8997189bc2b53b4596
ema-barr <emanuele.barracchia@hotmail.it>
2015-07-19 18:57:48 +0200

    Ripristinato metodo di connessione a mongo.

--- src/main/java/DAO.java
+++ src/main/java/DAO.java
@@ -4,7 +4,6 @@ import entity.ShortUrl;
 import org.bson.types.ObjectId;
 import utility.Constants;
 
-import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -68,9 +67,14 @@ public class DAO {
         return new ShortUrl((BasicDBObject) collection.findOne(new BasicDBObject("_id", new ObjectId(id))));
     }
 
-    private DB mongo() throws UnknownHostException {
+    private DB mongo() throws Exception {
         String host = System.getenv(Constants.ADDRESS_MONGO_CONNECTION);
+        if (host == null) {
+            MongoClient mongoClient = new MongoClient(Constants.ADDRESS_MONGO_CONNECTION);
+            return mongoClient.getDB(Constants.NAME_DB);
+        }
 
+        /* Codice non funzionante boot2docker
         if (host == null) {
             host = System.getenv(Constants.ADDRESS_MONGO_CONNECTION_BOOT2DOCKER);
             if (host == null){
@@ -84,6 +88,7 @@ public class DAO {
             }
 
         }
+         */
         int port = Integer.parseInt(Constants.PORT_DB);
         MongoClientOptions mongoClientOptions = MongoClientOptions.builder().build();
         MongoClient mongoClient = new MongoClient(new ServerAddress(host, port), mongoClientOptions);
